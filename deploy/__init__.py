f165479f6a7a8010acb7c66727e4d13f2bccc7f3
Huang <yaozeng.huang@acti.bot>
2026-01-26 14:35:08 +0800

    finish the deploy_mujoco but not test

---
+++ deploy/__init__.py
@@ -0,0 +1,4 @@
+import os
+
+# 项目根目录
+HIMLOCO_ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../../"))
--- deploy/deploy_mujoco/deploy_mujoco.py
+++ deploy/deploy_mujoco/deploy_mujoco.py
@@ -3,10 +3,10 @@ import time
 import mujoco.viewer
 import mujoco
 import numpy as np
-from legged_gym import LEGGED_GYM_ROOT_DIR
 import torch
 import yaml
 
+from .. import HIMLOCO_ROOT_DIR
 
 def get_gravity_orientation(quaternion):
     qw = quaternion[0]
@@ -24,22 +24,17 @@ def get_gravity_orientation(quaternion):
 
 
 def pd_control(target_q, q, kp, target_dq, dq, kd):
-    """Calculates torques from position commands"""
+    """PD 控制计算"""
     return (target_q - q) * kp + (target_dq - dq) * kd
 
 
 if __name__ == "__main__":
-    # get config file name from command line
-    import argparse
 
-    parser = argparse.ArgumentParser()
-    parser.add_argument("config_file", type=str, help="config file name in the config folder")
-    args = parser.parse_args()
-    config_file = args.config_file
-    with open(f"{LEGGED_GYM_ROOT_DIR}/deploy/deploy_mujoco/configs/{config_file}", "r") as f:
+    config_file = f"{HIMLOCO_ROOT_DIR}/deploy/deploy_mujoco/config/go2.yaml"
+    with open(config_file, "r") as f:
         config = yaml.load(f, Loader=yaml.FullLoader)
-        policy_path = config["policy_path"].replace("{LEGGED_GYM_ROOT_DIR}", LEGGED_GYM_ROOT_DIR)
-        xml_path = config["xml_path"].replace("{LEGGED_GYM_ROOT_DIR}", LEGGED_GYM_ROOT_DIR)
+        policy_path = config["policy_path"].replace("{HIMLOCO_ROOT_DIR}", HIMLOCO_ROOT_DIR)
+        xml_path = config["xml_path"].replace("{HIMLOCO_ROOT_DIR}", HIMLOCO_ROOT_DIR)
 
         simulation_duration = config["simulation_duration"]
         simulation_dt = config["simulation_dt"]
@@ -60,6 +55,7 @@ if __name__ == "__main__":
         num_obs = config["num_obs"]
         
         cmd = np.array(config["cmd_init"], dtype=np.float32)
+        max_cmd = np.array(config["max_cmd"], dtype=np.float32)
 
     # define context variables
     action = np.zeros(num_actions, dtype=np.float32)
@@ -68,30 +64,28 @@ if __name__ == "__main__":
 
     counter = 0
 
-    # Load robot model
+    # 加载机器人模型
     m = mujoco.MjModel.from_xml_path(xml_path)
     d = mujoco.MjData(m)
     m.opt.timestep = simulation_dt
 
-    # load policy
+    # 加载策略
     policy = torch.jit.load(policy_path)
 
     with mujoco.viewer.launch_passive(m, d) as viewer:
-        # Close the viewer automatically after simulation_duration wall-seconds.
         start = time.time()
         while viewer.is_running() and time.time() - start < simulation_duration:
             step_start = time.time()
             tau = pd_control(target_dof_pos, d.qpos[7:], kps, np.zeros_like(kds), d.qvel[6:], kds)
             d.ctrl[:] = tau
-            # mj_step can be replaced with code that also evaluates
-            # a policy and applies a control signal before stepping the physics.
+            # 可以在 mj_step 之前或之后应用控制信号
             mujoco.mj_step(m, d)
 
             counter += 1
             if counter % control_decimation == 0:
-                # Apply control signal here.
+                # 应用控制信号
 
-                # create observation
+                # 建立观测
                 qj = d.qpos[7:]
                 dqj = d.qvel[6:]
                 quat = d.qpos[3:7]
@@ -102,29 +96,22 @@ if __name__ == "__main__":
                 gravity_orientation = get_gravity_orientation(quat)
                 omega = omega * ang_vel_scale
 
-                period = 0.8
-                count = counter * simulation_dt
-                phase = count % period / period
-                sin_phase = np.sin(2 * np.pi * phase)
-                cos_phase = np.cos(2 * np.pi * phase)
-
-                obs[:3] = omega
-                obs[3:6] = gravity_orientation
-                obs[6:9] = cmd * cmd_scale
+                obs[:3] = cmd * cmd_scale * max_cmd
+                obs[3:6] = omega
+                obs[6:9] = gravity_orientation
                 obs[9 : 9 + num_actions] = qj
                 obs[9 + num_actions : 9 + 2 * num_actions] = dqj
                 obs[9 + 2 * num_actions : 9 + 3 * num_actions] = action
-                obs[9 + 3 * num_actions : 9 + 3 * num_actions + 2] = np.array([sin_phase, cos_phase])
                 obs_tensor = torch.from_numpy(obs).unsqueeze(0)
-                # policy inference
+                # 策略推理
                 action = policy(obs_tensor).detach().numpy().squeeze()
-                # transform action to target_dof_pos
+                # 把动作转换为目标关节位置
                 target_dof_pos = action * action_scale + default_angles
 
-            # Pick up changes to the physics state, apply perturbations, update options from GUI.
+            # 获取物理状态的变化，应用外部扰动，从GUI更新仿真参数
             viewer.sync()
 
-            # Rudimentary time keeping, will drift relative to wall clock.
+            # 基础的时间同步机制，长时间运行可能与实际时钟产生偏差
             time_until_next_step = m.opt.timestep - (time.time() - step_start)
             if time_until_next_step > 0:
                 time.sleep(time_until_next_step)
